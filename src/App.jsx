b482d79b0d9c9b99fd34f153b9d05149ae7fbdc9
vaishnavissantoji123-web <vaishnavissantoji123@gmail.com>
2026-01-22 14:23:25 +0530

    button added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button"
+
+
+
+
+function App() {
+
+  
+
+  return (
+    <>
+        <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button variant="outline">Button</Button>
+
+    </>
+  )
+}
+
+export default App
